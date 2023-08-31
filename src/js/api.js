14996655714e52730d7d6a6bd07d73a0f9baf49b
Iryna Burlaka <i.am.blinova@gmail.com>
2023-08-31 09:41:01 +0200

    api Functions for requests: - getBookCategories - getBooksByCategory - getTopFiveBooks - getBookById

---
+++ src/js/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://books-backend.p.goit.global/';
+
+async function getBookCategories() {
+  try {
+    const data = await axios.get(BASE_URL + 'books/category-list');
+
+    return data;
+  } catch (error) {
+    console.log('Something went wrong, please try again.');
+    return error;
+  }
+}
+// example
+// getBookCategories().then(res => console.log(res));
+
+async function getBooksByCategory(selectedCategory) {
+  try {
+    const data = await axios.get(
+      BASE_URL + `books/category?category=${selectedCategory}`
+    );
+    return data;
+  } catch (error) {
+    console.log('Something went wrong, please try again.');
+    return error;
+  }
+}
+// example
+// getBooksByCategory('Audio Nonfiction').then(res => console.log(res));
+
+async function getTopFiveBooks() {
+  try {
+    const books = await axios.get(BASE_URL + 'books/top-books');
+    return books;
+  } catch (error) {
+    console.log('Something went wrong, please try again.');
+    return error;
+  }
+}
+// example
+// getTopFiveBooks().then(res => console.log(res));
+
+async function getBookById(bookId) {
+  try {
+    const book = await axios.get(BASE_URL + `books/${bookId}`);
+    return book;
+  } catch (error) {
+    console.log('Something went wrong, please try again.');
+    return error;
+  }
+}
+// example
+// getBookById('643282b1e85766588626a0b4').then(res => console.log(res));
