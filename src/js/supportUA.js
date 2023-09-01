5ce3ea9a762ddbee107721ae1789d035c95153a4
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-09-01 15:46:24 +0200

    Update supportUA
    make fonds images .
    changed a little style.
    add an array with images

--- src/js/supportUA.js
+++ src/js/supportUA.js
@@ -1,49 +1,61 @@
 const arraySupports = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
-  },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: null,
-  },
-  {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
-  },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: null,
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/',
-    img: null,
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
-  },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en',
-    img: null,
-  },
+    {
+        title: 'Save the Children',
+        url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+        img: null,
+    },
+    {
+        title: 'Project HOPE',
+        url: 'https://www.projecthope.org/country/ukraine/',
+        img: null,
+    },
+    {
+        title: 'International Medical Corps',
+        url: 'https://internationalmedicalcorps.org/country/ukraine/',
+        img: null,
+    },
+    {
+        title: 'RAZOM',
+        url: 'https://www.razomforukraine.org/',
+        img: null,
+    },
+    {
+        title: 'Action against hunger',
+        url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+        img: null,
+    },
+    {
+        title: 'Serhiy Prytula Charity Foundation',
+        url: 'https://prytulafoundation.org/en',
+        img: null,
+    },
+    {
+        title: 'UNITED24',
+        url: 'https://u24.gov.ua/uk',
+        img: null,
+    },
+    {
+        title: 'Medicins Sans Frontieres',
+        url: 'https://www.msf.org/ukraine',
+        img: null,
+    },
+    {
+        title: 'World vision',
+        url: 'https://www.wvi.org/emergencies/ukraine',
+        img: null,
+    },
+];
+
+const arrayImg = [
+    { name: 'Save the Children', img: './image/SaveTheChildren.png' },
+    { name: 'Project HOPE', img: './image/projectHope.png' },
+    { name: 'International Medical Corps', img: './image/interMed.png' },
+    { name: 'UNITED24', img: './image/united24.png' },
+    { name: 'Medicins Sans Frontieres', img: './image/medSans.png' },
+    { name: 'RAZOM', img: './image/razom.png' },
+    { name: 'Action against hunger', img: './image/actionAH.png' },
+    { name: 'World vision', img: './image/worldV.png' },
+    { name: 'Serhiy Prytula Charity Foundation', img: './image/prytula.png' },
 ];
 
 const supCard = document.querySelector('.support-card');
@@ -53,63 +65,75 @@ const showAllButton = document.querySelector('.support-btn');
 const array = [];
 
 function formatNumber(number) {
-  return number.toLocaleString('en-US', {
-    minimumIntegerDigits: 2,
-    useGrouping: false,
-  });
+    return number.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
 }
 
-function renderSupport(arraySupports) {
-  let counter = 0;
+// const arraySupports = arraySupports.map()
 
-  supList.innerHTML = arraySupports
-    .map(
-      ({ title, url }) =>
-        `<li class="support-item"><a class="support-link link" href="${url}" target="_blank" rel="noopener noreferrer"><span class="support-numbers">${formatNumber(
-          (counter += 1)
-        )}</span>${title}
+function renderSupport(arraySupports, arrayImg) {
+    let counter = 0;
+
+    supList.innerHTML = arraySupports
+        .map(({ title, url }) => {
+            const imgInfo = arrayImg.find(imgObj => imgObj.name === title);
+            const imgSrc = imgInfo ? imgInfo.img : ''; // Отримуємо шлях до зображення
+
+            return `
+                <li class="support-item">
+                    <a class="support-link link" href="${url}" target="_blank" rel="noopener noreferrer">
+                        <span class="support-numbers">${formatNumber(
+                            (counter += 1)
+                        )}</span>
+                        <img class="img-support" src="${imgSrc}" alt="${title}" onclick="openLink('${url}')">
+                        
+                    </a>
                 </li>
-                </a>`
-    )
-    .join('');
+            `;
+        })
+        .join('');
 }
 // <img class="img-support" src="" alt="support UA" srcset="./image/saveTheChildren1.png">
 console.log(arraySupports);
-renderSupport(arraySupports);
+renderSupport(arraySupports, arrayImg);
 
 // Показати лише перші 6 елементів
 function showSixItems() {
-  const listItems = supList.querySelectorAll('.support-item');
-  listItems.forEach((item, index) => {
-    if (index >= 6) {
-      item.style.display = 'none';
-    }
-  });
+    const listItems = supList.querySelectorAll('.support-item');
+    listItems.forEach((item, index) => {
+        if (index >= 6) {
+            item.style.display = 'none';
+        }
+    });
 }
 showSixItems();
 
+// Показати лише перші 4 елементів
+
 showAllButton.addEventListener('click', onClick);
 function onClick(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const listItems = supList.querySelectorAll('.support-item');
+    const listItems = supList.querySelectorAll('.support-item');
 
-  listItems.forEach(item => {
-    item.style.display = 'block';
-  });
-  supCard.style.maxHeight = 'none'; // Розширення блоку
-  showAllButton.style.display = 'none'; // Ховаємо кнопку "Показати все"
+    listItems.forEach(item => {
+        item.style.display = 'block';
+    });
+    supCard.style.maxHeight = 'none'; // Розширення блоку
+    showAllButton.style.display = 'none'; // Ховаємо кнопку "Показати все"
 
-  showAllButton.removeEventListener('click', onClick); // Видалення обробника кліку після кліку
+    showAllButton.removeEventListener('click', onClick); // Видалення обробника кліку після кліку
 }
 
 document.addEventListener('click', onOutside);
 function onOutside(event) {
-  if (!supCard.contains(event.target)) {
-    // showAllButton.removeEventListener('click', onClick);
-    showAllButton.addEventListener('click', onClick);
+    if (!supCard.contains(event.target)) {
+        // showAllButton.removeEventListener('click', onClick);
+        showAllButton.addEventListener('click', onClick);
 
-    showSixItems();
-    showAllButton.style.display = 'block'; // Показуємо кнопку "Показати все"
-  }
+        showSixItems();
+        showAllButton.style.display = 'block'; // Показуємо кнопку "Показати все"
+    }
 }
