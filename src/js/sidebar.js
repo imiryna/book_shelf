7d4d15f3bffede856790fa84d3ff425d455f57eb
babubin <gleblutsko@gmail.com>
2023-09-02 12:18:57 +0300

    create book category

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,15 +1,50 @@
-import { getBookCategories } from './api.js';
+import { getBookCategories, getBooksByCategory } from './api.js';
 const sidebarList = document.querySelector('.sidebar-list');
+const categoryList = document.querySelector('.category-list');
 
 getBookCategories().then(({ data }) =>
-  sidebarList.insertAdjacentHTML('beforeend', createSidebarMarkup(data))
+  sidebarList.insertAdjacentHTML('beforeend', createCategoryList(data))
 );
 
-function createSidebarMarkup(data) {
+function createCategoryList(data) {
   return data
     .map(
       item =>
         `<li class="sidebar-item"><a href="" class="sidebar-link">${item.list_name}</a></li>`
     )
+    .join('');
+}
+
+sidebarList.addEventListener('click', categoryFilterBooks);
+function categoryFilterBooks(evt) {
+  if (evt.target.nodeName !== 'A') {
+    return;
+  }
+  evt.preventDefault();
+  const currentActiveLink = document.querySelector('.active');
+  currentActiveLink.classList.remove('active');
+  evt.target.classList.add('active');
+  getBooksByCategory(evt.target.textContent).then(
+    ({ data }) => (categoryList.innerHTML = createBooks(data))
+  );
+}
+function createBooks(arr) {
+  return arr
+    .map(item => {
+      function slicePhrase(phrase) {
+        if (phrase.length > 16) {
+          return phrase.slice(0, 16) + '...';
+        } else {
+          return phrase;
+        }
+      }
+      return `<li class="category-item" >
+
+      <img src="${item.book_image}" alt="${item.title}" class="book-img"/>
+      <h2 class="book-title">${slicePhrase(item.title)}</h2>
+      <p class="book-author">${item.author}</p>
+
+    </li>`;
+    })
     .join('');
 }
