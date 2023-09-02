dc59849795a6406084501ede4494d25678cda455
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-09-02 23:01:22 +0200

    added markup and classes and deleted the modal button

--- src/js/modalCard/modalCard.js
+++ src/js/modalCard/modalCard.js
@@ -1,23 +1,18 @@
 import { markupModal } from './templateCard.js';
 import { getBookById } from '../api.js';
 
-const modalCard = document.getElementById('modalCard');
-const openModalCard = document.getElementById('openModalCard');
 const modalWindow = document.getElementById('modalWindow');
 
 const LocalStorageKey = 'shopingList';
 
-const openModal = async () => {
+export const openModal = async id => {
   const localStorageData = checkLocaleStorege(LocalStorageKey);
 
-  // Отримав дані і записав в розмітку
-  const { data } = await getBookById('643282b1e85766588626a0b4');
-  console.log(data);
-  const templateCard = markupModal(data);
-  console.log(templateCard);
-  modalWindow.innerHTML = templateCard;
+  const { data } = await getBookById(id);
 
-  // Отримали доступ до кнопки
+  const templateCard = markupModal(data);
+
+  modalWindow.innerHTML = templateCard;
 
   const addBtn = document.getElementById('addBtn');
 
@@ -27,8 +22,6 @@ const openModal = async () => {
     : 'Add to shopping list';
 
   addBtn.addEventListener('click', () => {
-    console.log('click');
-    // toggleItemInLocStor(localStorageData, data);
     const newLocalStorageData = checkLocaleStorege(LocalStorageKey);
     toggleItemInLocStor(newLocalStorageData, data);
     const new1LocalStorageData = checkLocaleStorege(LocalStorageKey);
@@ -36,15 +29,14 @@ const openModal = async () => {
       new1LocalStorageData,
       data
     );
-    console.log(addBtn);
-    console.log(isItemInLocaleStoreg);
+
     addBtn.textContent = isItemInLocaleStoreg
       ? 'remove from the shopping list'
       : 'Add to shopping list';
   });
 };
 
-openModalCard.addEventListener('click', openModal);
+// openModal(book.id);
 
 function toggleItemInLocStor(storageData, data) {
   let list = [];
@@ -52,11 +44,9 @@ function toggleItemInLocStor(storageData, data) {
     const item = storageData.find(el => el._id === data._id);
     if (item) {
       list = [...storageData.filter(({ _id }) => _id !== data._id)];
-      console.log(list);
+
       localStorage.setItem(LocalStorageKey, JSON.stringify(list));
     } else {
-      //   Записуємо в зміну ліст попередні дані з локал сторидж через спред і до них пушимо цей айтем
-      // записуємо в локал сторидж
       list = [...storageData, item];
       localStorage.setItem(LocalStorageKey, JSON.stringify(list));
     }
