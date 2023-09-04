0fdca616b436b240b1043cae20b68467b2d284c3
TheyCallmeOleksii <DionodeKor@gmail.com>
2023-09-04 23:53:21 +0300

    6th Commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import '../src/js/sidebar.js';
 import '../src/js/themeSwitcher';
 import '../src/js/modalCard/modalCard';
 import '../src/js/supportUA';
-
+import '../src/js/auth-modal';
 
 import { markupTopCategoryBooks } from './js/preview';
 markupTopCategoryBooks();
