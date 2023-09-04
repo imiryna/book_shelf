9b0ade2e1bb8458986684b819a9aa0edb95a6973
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-09-04 17:09:30 +0300

    fixed error console

--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -6,21 +6,12 @@ const menuBars = document.getElementById('menu-bars');
 const DARK_THEME = 'dark';
 const LIGHT_THEME = 'light';
 
-function toggleDarkLightMode(isDark) {
-  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-  isDark
-    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
-    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-}
-
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', DARK_THEME);
-    toggleDarkLightMode(true);
     localStorage.setItem('theme', DARK_THEME);
   } else {
     document.documentElement.setAttribute('data-theme', LIGHT_THEME);
-    toggleDarkLightMode(false);
     localStorage.setItem('theme', LIGHT_THEME);
   }
 }
