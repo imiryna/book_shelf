fc09e091ba73d84a35497339b217714ab8aa50f5
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-09-05 21:24:44 +0200

    update footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,28 +1,31 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[modal-developers-open]'),
-      closeModalBtn: document.querySelector('[modal-developers-close]'),
-      modalDeveloper: document.querySelector('[data-modal-developer]'),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[modal-developers-open]'),
+    closeModalBtn: document.querySelector('[modal-developers-close]'),
+    modalDeveloper: document.querySelector('[data-modal-developer]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', openModalDeveloper);
+  refs.closeModalBtn.addEventListener('click', closeModalDeveloper);
+  refs.modalDeveloper.addEventListener('click', onBackdropClickDeveloper);
+
+  function openModalDeveloper() {
+    refs.modalDeveloper.classList.remove('is-hidden_developers');
+  };
+
+  function closeModalDeveloper() {
+    refs.modalDeveloper.classList.add('is-hidden_developers');
+  };
+
+  function onBackdropClickDeveloper(event) {
+    if (event.currentTarget === event.target) {
+      closeModalDeveloper();
+    }
+  };
   
-    refs.openModalBtn.addEventListener('click', openModalDeveloper);
-    refs.closeModalBtn.addEventListener('click', closeModalDeveloper);
-    refs.modalDeveloper.addEventListener('click', onBackdropClickDeveloper);
-  
-    function openModalDeveloper() {
-      refs.modalDeveloper.classList.remove('is-hidden_developers');
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      closeModalDeveloper();
     }
-    function closeModalDeveloper() {
-      refs.modalDeveloper.classList.add('is-hidden_developers');
-    }
-    function onBackdropClickDeveloper(event) {
-      if (event.currentTarget === event.target) {
-        closeModalDeveloper();
-      }
-    }
-    document.addEventListener('keydown', e => {
-      if (e.code === 'Escape') {
-        closeModalDeveloper();
-      }
-    });
-  })();
+  });
+})();
