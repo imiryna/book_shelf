2004413b900271c1617967fae170cb9417d89279
Iryna Burlaka <i.am.blinova@gmail.com>
2023-09-05 17:29:09 +0200

    registration/auth functionality

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,16 +1,67 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+export { registrationNewUser, loginUser, logOutUser };
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBdJw0Xntd-uafZX7X7jbpxjUot7W10l9E',
-  authDomain: 'book-shelf-865d9.firebaseapp.com',
-  projectId: 'book-shelf-865d9',
-  storageBucket: 'book-shelf-865d9.appspot.com',
-  messagingSenderId: '978730630697',
-  appId: '1:978730630697:web:74e50be3d40f14da7ea638',
-};
+import { alertError, alertSuccess } from './notifies';
+
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+// const email = 'i.am.blinova@gmail.com';
+// const password = '12345q6';
+const LS_USER_KEY = 'currentUser';
+
+function registrationNewUser(email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      const user = userCredential.user;
+      const { accessToken, email } = user;
+      localStorage.setItem(
+        LS_USER_KEY,
+        JSON.stringify({ accessToken: accessToken, email: email, name: 'Ira' })
+      );
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode == 'auth/email-already-in-use') {
+        alertError('Email already in use. Login with it or choose another one');
+      }
+    });
+}
+
+// registrationNewUser(email, password);
+
+function logOutUser() {
+  localStorage.removeItem(LS_USER_KEY);
+
+  signOut(auth).then(() => {
+    alertSuccess('Logout successfully');
+  });
+}
+
+function loginUser(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      const user = userCredential.user;
+      const { accessToken, email } = user;
+      localStorage.setItem(
+        LS_USER_KEY,
+        JSON.stringify({ accessToken: accessToken, email: email, name: 'Ira' })
+      );
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode == 'auth/wrong-password') {
+        alertError('Your password is wrong. Please, try again');
+      }
+    });
+}
+// loginUser(email, password);
--- src/js/notifies.js
+++ src/js/notifies.js
@@ -1,6 +1,10 @@
-export { alertError };
+export { alertError, alertSuccess };
 import { Notify } from 'notiflix';
 
 function alertError(text) {
   Notify.failure(text);
+}
+
+function alertSuccess(text) {
+  Notify.success(text);
 }
