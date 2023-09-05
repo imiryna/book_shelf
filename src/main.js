95bc898774032c7a4c0ca59644977b358c6a512c
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-09-05 18:26:15 +0200

    Create footer
    footer.html,
    footer.js,
    footer.scss

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import './js/mobile-nav-header';
 import './js/modalCard/modalCard';
 import './js/supportUA';
 import './js/auth';
-// import './js/auth-modal';
+import './js/auth-modal';
+import './js/footer'
 
 import { markupTopCategoryBooks } from './js/preview';
 markupTopCategoryBooks();
