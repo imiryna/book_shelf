bc835a7d2885d9c822a3ebd8f28fa07f4d29b142
Iryna Burlaka <i.am.blinova@gmail.com>
2023-09-07 10:21:11 +0200

    Some fixes of styles and top-book page

--- src/js/preview.js
+++ src/js/preview.js
@@ -9,7 +9,7 @@ function markupTopCategoryBooks() {
     topBooks.forEach(category => {
       const categoryBox = createCategoryBox(category);
       document
-        .querySelector('.js-books')
+        .querySelector('.top-book-list')
         .insertAdjacentHTML('beforeend', categoryBox);
     });
     addListenerToCards();
@@ -18,31 +18,43 @@ function markupTopCategoryBooks() {
 
 function createCategoryBox(category) {
   const bookList = category.books.map(book => createBookCard(book)).join('');
-  const categoryList = document.querySelector('.js-books');
-  const sidebarList = document.querySelector('.sidebar-list');
-  const currentActiveLink = document.querySelector('.active');
-
-  categoryList?.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    sidebarList.childNodes.forEach(child => {
-      child.childNodes.forEach(c => {
-        if (c.innerHTML === event.target.value) {
-          currentActiveLink.classList.remove('active');
-          c.classList.add('active');
-        }
-      });
-    });
-
-    getBooksByCategory(event.target.value).then(
-      ({ data }) => (categoryList.innerHTML = createBooks(data))
-    );
-  });
-
   return `
-   <div class="category-box">
-      <p class="name-category">${category.list_name}</p>
-      <ul class="js-list-books category-list-book">${bookList}</ul>
-        <button value="${category.list_name}" class="js-btn-books see-more">See more</button>
-      </div>`;
+<div class="category-box">
+    <p class="name-category">${category.list_name}</p>
+    <ul class="category-list">${bookList}</ul>
+    <div>
+      <button value="${category.list_name}" class="js-btn-books see-more">See more</button>
+    </div>
+        `;
 }
+
+// function createCategoryBox(category) {
+//   console.log(category.books);
+//   const bookList = category.books.map(book => createBookCard(book)).join('');
+//   const categoryList = document.querySelector('.js-books');
+//   const sidebarList = document.querySelector('.sidebar-list');
+//   const currentActiveLink = document.querySelector('.active');
+
+//   // categoryList?.addEventListener('click', function (event) {
+//   //   event.preventDefault();
+
+//   //   sidebarList.childNodes.forEach(child => {
+//   //     child.childNodes.forEach(c => {
+//   //       if (c.innerHTML === event.target.value) {
+//   //         currentActiveLink.classList.remove('active');
+//   //         c.classList.add('active');
+//   //       }
+//   //     });
+//   //   });
+
+//   //   getBooksByCategory(event.target.value).then(
+//   //     ({ data }) => (categoryList.innerHTML = createBooks(data))
+//   //   );
+//   // });
+//   return `
+//    <div class="category-box">
+//       <p class="name-category">${category.list_name}</p>
+//       <ul class="js-list-books category-list-book">${bookList}</ul>
+//         <button value="${category.list_name}" class="js-btn-books see-more">See more</button>
+//       </div>`;
+// }
